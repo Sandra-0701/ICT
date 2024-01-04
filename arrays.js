960eb97ebe07373b27ba25ef4f1783403879b703
Sandra-0701 <sandrasindhu0701@gmail.com>
2024-01-04 20:08:31 +0530

    first commit

---
+++ arrays.js
@@ -0,0 +1,28 @@
+var arr=[1,2,3,4,5]
+for(i=arr[0];i<=arr[4];i++){
+    console.log(i);
+
+}
+
+let ar=[1,2,3,4,5,6]
+for(i=0;i<ar.length;i++){
+    console.log(ar[i]);
+}
+
+let a=['a','b','c'];
+for(x of a){
+    console.log(x);
+}
+
+let person={name:'san'}
+for (x in person){
+    console.log(person[x]);
+}
+
+let j=1;
+while(j<=10){
+    console.log(j);
+    j++;
+}
+
+
